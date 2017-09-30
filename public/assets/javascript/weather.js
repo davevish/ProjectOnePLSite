9cbf89293dedae3bb5a3b8ff378c16176dfa7d56
Vishvesh Dave <vishvesh01@yahoo.com>
2017-09-30 14:24:54 -0400

    added files

---
+++ public/assets/javascript/weather.js
@@ -0,0 +1,25 @@
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?" +
+	"q=AFCBournemouth&units=imperial&appid=" + APIKey;
+
+// $(".thisTeamClass").on("click", function(){
+		$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+		.done(function(response) {
+
+			console.log(queryURL);
+			console.log(response);
+
+			$(".temp").html("Temperature: " + response.main.temp) + " F";
+			$(".weather").html(response.weather[0].description);
+			$(".wind").html("Wind Speed: " + response.wind.speed + " mph");
+
+			console.log("Wind Speed: " + response.wind.speed + " mph");
+			console.log("Weather: " + response.main.humidity);
+			console.log("Temperature: " + response.main.temp + " F");
+		});
+		console.log("this");
+// });
