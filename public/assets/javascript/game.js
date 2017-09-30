4f77717823b586ea24a327119c6c3c24863b43d2
alexdecycive <alex@decycive.com>
2017-09-30 13:51:06 -0400

    Added game design elements, added working game JS, needs DB hooks

--- public/assets/javascript/game.js
+++ public/assets/javascript/game.js
@@ -26,22 +26,46 @@ function squarereset() {
 }
 console.log(goalsquares);
 
+$('.gamediv').each(function(index) {
+	// if index is included inside goalsquares
+	console.log(index)
+	if (goalsquares.includes(index))	 {
+		$(this).attr("goal", "Yes");
+	  // apply goal atrribute
+	} else {
+		$(this).attr("goal", "No");
+	}
+	//end if
+})
 
 //on click of a square, see if it's a goal or block
 $(".gamediv").on("click", function () {
-	//if (div.id == ) {
+	if ( $(this).attr("goal") == "Yes" ) {
 		goals++;
 		pksremaining--;
-	//} else {
-		//blocks++;
-		//pksremaining--;
-	//}
+	} else {
+		blocks++;
+		pksremaining--;
+	}
 
 	//Update goals, blocks, and PKs
 	$("#goals-div").text(goals);
+	$("#blocks-div").text(blocks);
 	$("#pks-div").text(pksremaining);
 	console.log("GOALS:" + goals)
 	console.log("PKS:" + pksremaining)
+
+	if (pksremaining == 0) {
+		if (goals > blocks) {
+			$("#winnermodal").modal("toggle");
+			console.log("Winner egg called");
+		} else {
+			$("#losermodal").modal("toggle");
+			console.log("Loser egg called");
+		}
+		
+		reset();
+	}
 });
 
 
@@ -51,15 +75,27 @@ $(".gamediv").on("click", function () {
 function reset() {
 	$("#pks-div").text(5);
 	$("#goals-div").text(0);
-	$("#goals-div").text(0);
+	$("#blocks-div").text(0);
+	blocks = 0;
+	goals = 0;
+	pksremaining = 5;
 }
 
 $("#resetbtn").on("click", function() {
 	event.preventDefault();
 	$("#pks-div").text(5);
 	$("#goals-div").text(0);
-	$("#goals-div").text(0);
+	$("#blocks-div").text(0);
+	blocks = 0;
+	goals = 0;
+	pksremaining = 5;
 });
+
+//jumbotron collapse
+$("#jumbo-btn").on("click", function() {
+	$("#header-section").html("");
+	//$("#jumbotron-div").attr("style", "height:10px;")
+})
  
 
 //End the doc inialize func
